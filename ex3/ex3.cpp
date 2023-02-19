d6e52c84c57a2d4c6baf568612ba82412b0a9cb6
Vitaliy <andrusishin.vit@gmail.com>
2023-02-19 22:36:11 +0200

    Добавьте файлы проекта.

---
+++ ex3/ex3.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+#include <Windows.h>
+
+using namespace std;
+
+int main() {
+    // оголошення змінних
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+    string product = "нові наушники";
+    float price = 499.99;
+    string seller = "ООО Техніка";
+    string phone = "+380123456789";
+
+    // виведення оголошення на екран
+    cout << "** ОГОЛОШЕННЯ **\n\n";
+    cout << "Продаю " << product << " за ціною " << price << " грн.\n";
+    cout << "Продавець: " << seller << "\n";
+    cout << "Телефон: " << phone << "\n\n";
+    cout << "**\n";
+
+    return 0;
+}
