c38042c6abd67ec39e827ba61945e9c0b6a13caf
vitaliyandr <116282877+vitaliyandr@users.noreply.github.com>
2023-02-19 22:36:41 +0200

    Add files via upload

---
+++ ex3/ex1.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+#include <Windows.h>
+#include <locale.h>
+#include <stdio.h>
+using namespace std;
+
+int main() {
+    cout << "У полi бiля шляху калина,\n"
+        << "Нiби дiвчина, спiває сама.\n"
+        << "Вiтерець дзвiнко грає з нею,\n"
+        << "Мов оживає, мов золота дитина.\n\n";
+
+    // зазначення виконавця пісні
+    cout << " - Українська народна пiсня\n";
+
+    return 0;
+}
---
+++ ex3/ex2.cpp
@@ -0,0 +1,12 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "\t\t\tEvery hunter wants\n\n";
+	cout << "\t\tA gray elephant with a 6-foot tusk\n";
+	cout << "\t\tAnd a blue baboon with a pink tail,\n";
+	cout << "\t\tA green gorilla with bright orange spots,\n";
+	cout << "\t\tAnd a black and white striped, long-necked hippopotamus.\n";
+
+	return 0;
+}
